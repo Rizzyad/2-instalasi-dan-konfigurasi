e88d17f046d422f718badbf528f8ccb1111a336b
rizzyad <rizzyadramadhan@gmail.com>
2024-05-27 19:37:32 +0700

    add folder components to separate the Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
-
-function Header({ author }) {
-  return <h1>Belajar React bareng {author ? author : 'WPU'} 🚀</h1>
-}
+import Header from './components/Header';
 
 function App() {
   const [likes, setLikes] = useState(0);
