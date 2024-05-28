779953ebd9ee1d10b2da5f23367ad054fc4779d1
rizzyad <rizzyadramadhan@gmail.com>
2024-05-28 17:43:33 +0700

    counter klik dasar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import Header from './components/Header';
 
 function App() {
   const [likes, setLikes] = useState(0);
+  const [showMessage, setShowMessage] = useState(false);
 
   function handleClick() {
     setLikes(likes + 1);
   }
 
+  function toggleShowMessage() {
+    setShowMessage(prevShowMessage => !prevShowMessage);
+  }
+
+  const cek = "ini variabel";
+
+  const tulisan = `cek tulisan ${cek} `;
+
   return (
     <div>
       <Header author={"Risyad"} />
       <button onClick={handleClick}>Like ({likes})</button>
+      <button onClick={toggleShowMessage}>Tambah</button>
+      {showMessage ? <h1>Tombol diklik</h1> : <h1>Tombol tidak diklik</h1>}
+      <h1> adalah {tulisan}</h1>
     </div>
   );
 }
 
-export default App
+export default App;
